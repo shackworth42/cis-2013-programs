78cd14bc2e93b715e79a5a806c2bf03860bd7955
shackworth42 <122641460+shackworth42@users.noreply.github.com>
2023-02-03 13:19:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var floatMiles, floatGallons, floatMpg, strMessage;
 floatMiles= prompt('Enter Miles Driven');
 floatGallons= prompt('Enter Gallons Used');
-floatMpg= parseFloat(floatMiles/floatGallons).toFixed(2)
+floatMpg= parseFloat(floatMiles/floatGallons).toFixed(4)
 /*
 if (floatMpg < 10) {strMessage= 'IN THIS ECONOMY?.'}
 if (floatMpg ) {strMessage= 'A true American.'}
