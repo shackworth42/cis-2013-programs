5ea1ec770c2c4061fe36a42a372ca83e11db00e0
shackworth42 <122641460+shackworth42@users.noreply.github.com>
2023-02-03 13:03:53 -0600

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
 floatAge=prompt("How old are you in years?")
-floatDays=(floatAge*365.25)
-intWeeks=parseInt(floatAge*52)
-floatMonths=parseFloat(floatAge*12)
-intFortnights=parseInt(floatDays/14)
-alert("You are: " + floatDays +  ' Days Old' + '\n' + intWeeks + ' Weeks Old' + '\n' + floatMonths + ' Months Old' + '\n' + intFortnights + ' Fortnights Old! Wow thats really old...')
+floatDays=(floatAge*365.25).toFixed(2)
+intWeeks=parseInt(floatAge*52).toFixed(2)
+floatMonths=parseFloat(floatAge*12).toFixed(2)
+intFortnights=parseInt(floatDays/14).toFixed(2)
+alert("You are: " + '\n' + floatDays +  ' Days Old' + '\n' + intWeeks + ' Weeks Old' + '\n' + floatMonths + ' Months Old' + '\n' + intFortnights + ' Fortnights Old! Wow thats really old...')
