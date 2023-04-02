ff1850c60285fb3bf2c69dc0cd8437e58220657a
shackworth42 <122641460+shackworth42@users.noreply.github.com>
2023-04-02 11:06:27 -0500

    redesign
    basically rewrote the program 20 times

--- HO8/betterguess.js
+++ HO8/betterguess.js
@@ -2,6 +2,7 @@ window.onload = function () {
 var $ = function(id) { return document.getElementById(id); };
 $("start").onclick = game;//start 
 $("quit").onclick = quit;//quit function
+$("min").focus();//focus on min input box
 };
 
 var quit = function() {
@@ -12,20 +13,33 @@ var quit = function() {
 var game = function() {
     
     
-    var intRandom, intCount, intGuess, intGuesses, intScore, intMin, intMax, intSentinel = 999;//set sentinel value and dec all vars
+    var intRandom, intCount, intGuess, intGuesses, intScore, intMin, intMax, intSentinel = -1;//set sentinel value and dec all vars
 
 
     var quit = function() {
         window.location.href = "https://shackworth42.github.io/cis-2013-programs/HO8/stare.jpg";
     }
     var $ = function(id) { return document.getElementById(id); };
-    var intMin = parseInt($("min").value);
-    var intMax = parseInt($("max").value);//re declare important vars
+    var intMin = (parseInt($("min").value)) + 1;//makes it so the max and min arent included
+    var intMax = (parseInt($("max").value)) - 1;//re declare important vars bc im paranoid now after geometry functions
 
 
+if (intMin == intSentinel) {
+        quit();
+    }
+
+if (intMin == '' || intMax == '') {
+    alert("You must enter a number for both the minimum and maximum range.");
+    return;
+}
+
+if (intMin < 0 || intMax < 0) {
+    alert("You must enter a positive number for both the minimum and maximum range.");
+    return;
+}
 
 if (isNaN(intMin) || isNaN(intMax)) {
-    alert("You must enter a number for both the minimum and maximum range.");
+    alert("You must enter numbers for both the minimum and maximum range.");
     return;
 }
 
@@ -33,125 +47,71 @@ if (intMin > intMax) {
     alert("The minimum range must be less than the maximum range.");
     return;
 }
-//small verification steps
+
+if (intMax - intMin == 0) {
+    alert("Have some fun and enter a range greater than 1.");
+    return;
+}
+//verification steps
 
 
 if (intMin == intSentinel) {//sentinel if, duh
     quit();
 }
-//my spacing and grouping here is atrocious i wish a professional could see whatever mess this is
-  
+
     intRandom = parseInt(Math.floor(Math.random() * (intMax - intMin + 1)) + intMin);//pray the random number generator works
     //for some reason im testing this a day later and it works 100% of the time now. I dont know why.
-    
+    //now i have to add and subtract 1 to the min and max to make sure the random number is in the range bc im too scared to change this
     intCount = 0;
     
-    intGuess = prompt("Your score is determined by a function of your range and your attempts. \n Please enter your guess between " + intMin + " and " + intMax + ": ");
-    
-    if (isNaN(intGuess) || intGuess < intMin || intGuess > intMax) {
-        alert("You must enter a number between " + intMin + " and " + intMax);
-    }
-    
-    while (intGuess != intRandom) {
-        switch (true) {
-            case (intGuess < intRandom):
-                alert("Your guess is too low.");
-                break;
-            case (intGuess > intRandom):
-                alert("Your guess is too high.");
-                break;
-        case (intGuess > intMax || intGuess < intMin):
-            alert("You must enter a number between " + intMin + " and " + intMax);
+    intGuess = prompt("Please enter your guess between " + (intMin - 1) + " and " + (intMax + 1) + ": ");
+    //prompt for guess
+if (intGuess == intSentinel) {
+    quit();
+}
+if (intGuess < intMin || intGuess > intMax) {
+   intGuess = prompt("Your guess must be between " + (intMin - 1) + " and " + (intMax + 1) + ".");
+}
+
+
+    while (intGuess != intRandom) {//main game loop
+        if (intGuess = '') {
             break;
         }
+    
+        else if (intGuess < intRandom) {
+            intGuess = prompt("Your guess is too low. Please guess again: ");
+        } else if (intGuess > intRandom) {
+            intGuess = prompt("Your guess is too high. Please guess again: ");
+        }
+        else if (intGuess == 20) {
+            intGuess = prompt("This is a little embarassing, isnt it?");
+        }
+        else if (intGuess == intSentinel) {
+            quit();
+        }
+
+
         intCount++;
-        intGuesses = intCount + 1;
     }
+    intGuesses = intCount + 1;
+    //core game loop
+   
     
-    if ((intMax - intMin) < 20 ) { //collassal if statement for score, if the difference between the max and min is less than 20, there is a harsher grading system
-        switch (intGuesses) {//i enjoy writing switch statements, i understand its ugly
-        case 1:
-            intScore = 100;
-            break;
-        case 2:
-            intScore = 90;
-            break;
-        case 3:
-            intScore = 80;
-            break;
-        case 4:
-            intScore = 70;
-            break;
-        case 5:
-            intScore = 60;
-            break;
-        case 6:
-            intScore = 50;
-            break;
-        case 7:
-            intScore = 40;
-            break;
-        case 8:
-            intScore = 30;
-            break;
-        case 9:
-            intScore = 20;
-            break;
-        case 10:
-            intScore = 10;
-            break;
-        default:
-            intScore = 0;
-        }
+    if ((intMax - intMin) < 20 ) {
+        intScore = (100 - (intGuesses * 5));
+    } else if ((intMax - intMin) < 50) {
+        intScore = (150 - (intGuesses * 7));
     } else {
-        switch (intGuesses) {
-        case 1:
-            intScore = 100;
-            break;
-        case 2:
-            intScore = 95;
-            break;
-        case 3:
-            intScore = 90;
-            break;
-        case 4:
-            intScore = 85;
-            break;
-        case 5:
-            intScore = 80;
-            break;
-        case 6:
-            intScore = 75;
-            break;
-        case 7:
-            intScore = 70;
-            break;
-        case 8:
-            intScore = 65;
-            break;
-        case 9:
-            intScore = 60;
-            break;
-        case 10:
-            intScore = 55;
-            break;
-        case 11:    
-            intScore = 50;
-            break;
-        default:
-            intScore = 0;
-        }
-   
+        intScore = (200 - (intGuesses * 10));
     }
-   
-   
    
 
         alert("Congratulations!!! You guessed the correct number (" + intRandom + ").\n" +
         "You got a score of " + intScore + " in " + intGuesses + " attempts.\n" +
-        "Play again to improve your score or enter " + intSentinel + " into the minimum range to quit.");
+        "Play again to improve your score or enter " + intSentinel + " into the minimum range to quit. \n You can also enter the sentinel into the guess box to quit at any time.");
 
         $("min").value = '';//nulls out input boxes. not sure if i could do them earlier but here is safest
         $("max").value = '';
-   
+        $("min").focus();//focuses on min input box
 }
